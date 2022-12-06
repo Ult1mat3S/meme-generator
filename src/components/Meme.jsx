406d815465a1f8c4e49e270b86efc00cd593619c
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-12-06 02:22:08 -0600

    feat: update to use and display input, add styling

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,7 +5,7 @@ export default function Meme() {
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg",
+    randomImage: "https://i.imgflip.com/1bij.jpg",
   });
 
   const [allMemeImages, setAllMemeImages] = useState(memesData);
@@ -20,27 +20,43 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <>
       <main>
         <form className="form">
           <input
             type="text"
-            name="form-input"
-            id="form-input"
-            placeholder="Text"
+            name="topText"
+            id="topText"
+            placeholder="Enter top text"
+            value={meme.topText}
+            onChange={handleChange}
           />
           <input
             type="text"
-            name="form-input"
-            id="form-input"
-            placeholder="Text"
+            name="bottomText"
+            id="bottomText"
+            placeholder="Enter bottom text"
+            value={meme.bottomText}
+            onChange={handleChange}
           />
           <button type="button" onClick={getRandomMeme} className="form-button">
             Get a new meme image 🖼️
           </button>
         </form>
-        <img src={meme.randomImage} alt="" className="meme-image" />
+        <div className="meme">
+          <img src={meme.randomImage} alt="" className="meme-image" />
+          <h2 className="meme-text top">{meme.topText}</h2>
+          <h2 className="meme-text bottom">{meme.bottomText}</h2>
+        </div>
       </main>
     </>
   );
