e28f1e293b8bba31b3b8d97ad75c021e7264cdd0
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-12-09 03:08:37 -0600

    fix: rename image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import icon from "../assets/TrollFace.png";
+import TrollFace from "../assets/TrollFace.png";
 
 export default function Header() {
   return (
     <>
       <header className="header">
-        <img src={icon} alt="" className="header-image" />
+        <img src={TrollFace} alt="" className="header-image" />
         <h2 className="header-title">Meme Generator</h2>
         <p className="header-name">React Course - Project 3</p>
       </header>
