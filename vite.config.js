aad084ef3969515fc6ca990da9019863814d1522
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-12-09 03:37:52 -0600

    fix: cache issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/meme-generator/",
@@ -9,6 +11,13 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`,
+      },
+    },
     emptyOutDir: true,
   },
 });
